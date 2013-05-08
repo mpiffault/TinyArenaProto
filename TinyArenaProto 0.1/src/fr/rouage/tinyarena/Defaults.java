fec82d61ea8c8f45fa0498327146d5b70024eacc
mpiffault <martin.piffault@gmail.com>
2013-05-08 14:11:12 +0200

    Added .classpath, modified Defaults.java to add walls.

--- TinyArenaProto 0.1/src/fr/rouage/tinyarena/Defaults.java
+++ TinyArenaProto 0.1/src/fr/rouage/tinyarena/Defaults.java
@@ -6,6 +6,6 @@ public class Defaults
 	public static int CELL_SIDE;
 	public static boolean grid = false;
 	public static float SENSITIVITY = 40.0f;
-	public static float WALLS_PERCENT = 0.0f;
+	public static float WALLS_PERCENT = 0.2f;
 	public static final String PREFS_NAME = "TinyPrefs";
 }
